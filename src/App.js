9f235cadd896000ffc0bcbe3049b4352c7da1aae
Halid_Macalangcom <vmhunterjob@gmail.com>
2022-04-04 21:20:41 +0800

    63 Comparing the cards if matched

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import SingleCard from "./components/SingleCard";
 
@@ -29,9 +29,27 @@ function App() {
 
   // handle a choice
   const handleChoice = (card) => {
-    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
   };
+  //compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        console.log("match card");
+        resetTurn();
+      } else {
+        console.log("not match card");
+        resetTurn();
+      }
+    }
+  }, [choiceOne, choiceTwo]);
 
+  //reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns(prevTurns => prevTurns + 1);
+  };
   return (
     <div className="App">
       <h1>Magic Match</h1>
